418ebbd6783317914fc161e7e75575c9d4487b49
JJTChannel <95204359+JJTChannel@users.noreply.github.com>
2023-01-17 20:25:15 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local hwid = game:GetService("RbxAnalyticsService"):GetClientId() -- to get your hwid Execute this setclipboard(game:GetService("RbxAnalyticsService"):GetClientId())
 
-local check = game:HttpGet("Check Hwid Link" .. hwid) -- The php code is in index.php
+local check = game:HttpGet("<Check Link>?key=" .. hwid) -- The php code is in index.php
 
 if check == "Whitelisted" then
 print("Whitelisted hwid") -- put script here
